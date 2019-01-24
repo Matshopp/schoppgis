e95268c9b98a89bdff77ea538e80bd22f7d40c0c
Matshop <Mouton114>
2019-01-24 19:44:16 +0100

    portfolio_filter -> portfolio

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name='portfolio'
 urlpatterns=[
     path(r'', views.home, name='home'),
-    path(r'portfolio', views.portfolio_category_list, name='portfolio_filter'),
+    path(r'portfolio/', views.portfolio_category_list, name='portfolio'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
 ]
