8ebb886f7b8d3c12a477eb6a9e4bde41846f47cc
Matshop <Mouton114>
2019-01-24 19:50:57 +0100

    quick and dirty url redirect

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name='portfolio'
 urlpatterns=[
     path(r'', views.home, name='home'),
+    path(r'a-propos/', views.home),
     path(r'portfolio/', views.portfolio_category_list, name='portfolio'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
 ]
